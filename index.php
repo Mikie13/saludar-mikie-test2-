cfd4a044d92e3ad3e8d815ba7bea4a07f6fe0f37
Mikie13 <mikiesaludar6@gmail.com>
2024-11-06 23:13:08 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,11 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>My first Ubuntu Server PHP Deployment</title>
 </head>
 <body>
+
+	<p>My first Ubuntu Server PHP Deployment</p>
 
 </body>
 </html>
